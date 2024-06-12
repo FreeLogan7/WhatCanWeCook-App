851a0a56e1b6c57c8fdbf444ea405565ddfc484f
freelogan7 <logan.freedman@outlook.com>
2024-06-11 22:39:56 -0400

    Remove line

--- app/src/main/java/com/freedman/whatcanweeat/MainActivity.kt
+++ app/src/main/java/com/freedman/whatcanweeat/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() , RecipesFragment.NewActivityListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // Temporary line
 
         binding.pager.adapter = PagerAdapter(this)
         TabLayoutMediator(binding.tabLayout, binding.pager) { tab, i ->
